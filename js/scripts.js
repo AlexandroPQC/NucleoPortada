569b4c7e9d8a73638f3db2f87148bea8425388ad
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2016-07-29 17:10:35 -0400

    Assets y estilos agregados.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 
+$(document).ready(function(){
+      $(".button-collapse").sideNav();
 
-$(".button-collapse").sideNav();
+      $('.slider').slider({full_width: true});
+      $('.slider').slider({height: 450});
+});
